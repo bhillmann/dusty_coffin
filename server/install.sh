70115917d56da60e18647f5ed76204d0bc08545b
Ben Hillmann <bhillmann@outlook.com>
2015-03-05 22:55:57 -0600

    Committing for fetch.

--- server/install.sh
+++ server/install.sh
@@ -1,3 +1,3 @@
 docker pull dockerfile/elasticsearch
 docker build -t bhillmann/nginx nginx
-docker build -t bhillmann/dusty_coffin dusty_coffin
+# docker build -t bhillmann/dusty_coffin dusty_coffin
