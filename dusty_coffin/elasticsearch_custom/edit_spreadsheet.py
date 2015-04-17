4f7787732f784647d7b084a162c6267fae346d77
Benjamin Hillmann <bhillmann@outlook.com>
2015-04-17 16:34:14 -0500

    Fixed the edit spreadsheet.

--- dusty_coffin/elasticsearch_custom/edit_spreadsheet.py
+++ dusty_coffin/elasticsearch_custom/edit_spreadsheet.py
@@ -3,20 +3,18 @@ __author__ = 'bhillmann'
 import urllib.request
 from io import BytesIO
 import pandas as pd
-import json
+import ujson
 from elasticsearch import Elasticsearch
 
 csv = urllib.request.urlopen("https://docs.google.com/spreadsheet/pub?key=0Ahf71UaPpMOSdGl0NnQtSFgyVFpvSmV3R2JobzVmZHc&output=csv").read()
 bio = BytesIO(csv)
 csv_pd = pd.DataFrame.from_csv(bio)
-csv_pd.shape
 csv_pd.columns = ["title", "username", "description", "subject_areas", "type", "locations", "tag_words", "discipline", "url", "creator", "time_periods", "alternate_title", "source_type", "keywords", "macalester_dataset"]
-json_objs = csv_pd.reset_index().to_json(orient='index')
-d = json.loads(json_objs)
+json_objs = csv_pd.reset_index().to_json(orient='records')
+dict_array= ujson.loads(json_objs)
 es = Elasticsearch('ec2-52-10-17-100.us-west-2.compute.amazonaws.com:9200')
 # ignore 404 and 400
 es.indices.delete(index='datasets_index', ignore=[400, 404])
-for key in d:
-    print(d[key])
-    res = es.index(index="datasets_index", doc_type="dataset", body=d[key])
+for d in dict_array:
+    res = es.index(index="datasets_index", doc_type="dataset", body=d)
     print(res['created'])
