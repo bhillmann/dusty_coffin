9d3624ad8cf3d257f6875c77fa06a8c924feb40a
Benjamin Hillmann <bhillmann@outlook.com>
2015-04-19 14:06:51 -0500

    Created the new web app.

---
+++ crud/app/scripts/main.js
@@ -0,0 +1,68 @@
+var editor; // use a global for the submit and return data rendering in the examples
+
+$(document).ready(function() {
+    editor = new $.fn.dataTable.Editor( {
+        ajax: {
+            create: {
+                type: 'POST',
+                url:  '../php/rest/create.php'
+            },
+            edit: {
+                type: 'PUT',
+                url:  '../php/rest/edit.php?id=_id_'
+            },
+            remove: {
+                type: 'DELETE',
+                url:  '../php/rest/remove.php?id=_id_'
+            }
+        },
+        table: "#example",
+        fields: [ {
+                label: "First name:",
+                name: "first_name"
+            }, {
+                label: "Last name:",
+                name: "last_name"
+            }, {
+                label: "Position:",
+                name: "position"
+            }, {
+                label: "Office:",
+                name: "office"
+            }, {
+                label: "Extension:",
+                name: "extn"
+            }, {
+                label: "Start date:",
+                name: "start_date"
+            }, {
+                label: "Salary:",
+                name: "salary"
+            }
+        ]
+    } );
+
+    $('#example').DataTable( {
+        dom: "Tfrtip",
+        ajax: "../php/rest/get.php",
+        columns: [
+            { data: null, render: function ( data, type, row ) {
+                // Combine the first and last names into a single table field
+                return data.first_name+' '+data.last_name;
+            } },
+            { data: "position" },
+            { data: "office" },
+            { data: "extn" },
+            { data: "start_date" },
+            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
+        ],
+        tableTools: {
+            sRowSelect: "os",
+            aButtons: [
+                { sExtends: "editor_create", editor: editor },
+                { sExtends: "editor_edit",   editor: editor },
+                { sExtends: "editor_remove", editor: editor }
+            ]
+        }
+    } );
+} );
