2b74cb8f0ec5b19555f614d397e813eb2d44b061
Benjamin Hillmann <bhillmann@outlook.com>
2015-04-19 14:12:31 -0500

    Updated the port for the gruntfile.

--- crud/Gruntfile.js
+++ crud/Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9100,
+        port: 9101,
         open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
