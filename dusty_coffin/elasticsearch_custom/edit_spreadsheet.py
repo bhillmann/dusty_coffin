e6873d3d40d868e743a239c6a74a345c2999541e
Benjamin Hillmann <bhillmann@outlook.com>
2015-05-06 17:02:49 -0500

    Edit spreadsheet is now fixed.

--- dusty_coffin/elasticsearch_custom/edit_spreadsheet.py
+++ dusty_coffin/elasticsearch_custom/edit_spreadsheet.py
@@ -4,9 +4,10 @@ import pandas as pd
 import ujson
 from elasticsearch import Elasticsearch
 
-csv = urllib.request.urlopen("https://docs.google.com/spreadsheet/pub?key=0Ahf71UaPpMOSdGl0NnQtSFgyVFpvSmV3R2JobzVmZHc&output=csv").read()
+csv = urllib.request.urlopen("https://docs.google.com/spreadsheet/pub?key=1h1udf_H073YaVlZs0fkYUf9dC6KbEZAhF1veeLExyXo&gid=937170620&output=csv").read()
 bio = BytesIO(csv)
 csv_pd = pd.DataFrame.from_csv(bio)
+
 json_objs = csv_pd.reset_index().to_json(orient='records')
 dict_array= ujson.loads(json_objs)
 
@@ -16,5 +17,6 @@ es = Elasticsearch('ec2-52-10-17-100.us-west-2.compute.amazonaws.com:9200')
 # ignore 404 and 400
 es.indices.delete(index='datasets_index', ignore=[400, 404])
 for d in dict_array:
-    res = es.index(index="datasets_index", doc_type="dataset", body=d)
-    print(res['created'])
+	print(d)
+	res = es.index(index="datasets_index", doc_type="dataset", body=d)
+	print(res['created'])
