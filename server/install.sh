590de893633599a57ca5f77203b987975815e475
Benjamin Hillmann <bhillmann@outlook.com>
2015-05-20 13:20:41 -0500

    Updated the install.sh

--- server/install.sh
+++ server/install.sh
@@ -2,7 +2,6 @@
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
 
-sudo docker pull dockerfile/elasticsearch
 sudo docker build -t bhillmann/elasticsearch-proxy elasticsearch-proxy
 sudo docker build -t bhillmann/admin admin
 sudo docker build -t bhillmann/frontend frontend
